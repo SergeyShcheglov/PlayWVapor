d4271ac93b3574582ce1cea1a71b4ba03e1cbf0d
Sergey Shcheglov <97shcheglov@gmail.com>
2022-11-03 19:44:23 +0100

    add post route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,8 +9,24 @@ func routes(_ app: Application) throws {
         "Hello, world!"
     }
 
-    app.get("Sam") { req async -> String in
-        "Hello, Sam!"
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        return "Hello, \(name)"
     }
     
+    app.post("info") { req -> InfoResponse in
+      let data = try req.content.decode(InfoData.self)
+      return InfoResponse(request: data)
+    }
+}
+
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+ let request: InfoData
 }
